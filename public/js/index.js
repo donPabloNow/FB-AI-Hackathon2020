f5b984d15ac62fa1713276983d7c43031483febb
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-05-20 15:55:29 -0400

    audio recording saved in browser

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,72 @@ function msToHMS( ms ) {
 }
 
 
+var constraints= {audio:true};
+var chunks = [];
+navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
+  var audio = document.querySelector('audio');
+  audio.srcObject = mediaStream;
+  var mediaRecorder = new MediaRecorder(mediaStream);
+
+  record.onclick = function() {
+    mediaRecorder.start();
+    console.log(mediaRecorder.state);
+    console.log("recorder started");
+    record.style.background = "red";
+    record.style.color = "black";
+  }
+  var stop = document.getElementById('stop');
+  stop.onclick = function() {
+    mediaRecorder.stop();
+    console.log(mediaRecorder.state);
+    console.log("recorder stopped");
+    record.style.background = "";
+    record.style.color = "";
+  }
+
+  mediaRecorder.onstop = function(e) {
+    console.log("data available after MediaRecorder.stop() called.");
+
+    var clipName = prompt('Enter a name for your sound clip');
+
+    var clipContainer = document.createElement('article');
+    var clipLabel = document.createElement('p');
+    var audio = document.createElement('audio');
+    var deleteButton = document.createElement('button');
+   
+    clipContainer.classList.add('clip');
+    audio.setAttribute('controls', '');
+    deleteButton.innerHTML = "Delete";
+    clipLabel.innerHTML = clipName;
+
+    clipContainer.appendChild(audio);
+    clipContainer.appendChild(clipLabel);
+    clipContainer.appendChild(deleteButton);
+    soundClips.appendChild(clipContainer);
+
+    audio.controls = true;
+    var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+    chunks = [];
+    var audioURL = URL.createObjectURL(blob);
+    audio.src = audioURL;
+    console.log("recorder stopped");
+
+    deleteButton.onclick = function(e) {
+      evtTgt = e.target;
+      evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+    }
+  }
+
+  mediaRecorder.ondataavailable = function(e) {
+    chunks.push(e.data);
+    console.log(chunks);
+    socket.emit('audio', chunks);
+  }
+ // audio.play();
+}).catch(function(err) {
+  console.log(err.message);
+})
+
 app.controller("mainController", ['$scope','$http','$sce', function($scope, $http, $sce) {
   $scope.view = 0;
   $scope.currid = "home";
