3a5513894dcd5ddfc918432d410a05397e307ad9
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-05-23 08:06:16 -0400

    audio recording fixed

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 var app = angular.module("myApp", []);
 var socket = io();
 function randomIntFromInterval(min, max) { // min and max included 
@@ -107,34 +108,56 @@ app.controller("mainController", ['$scope','$http','$sce', function($scope, $htt
   }
 
   var mic = new Wit.Microphone(document.getElementById("microphone"));
-	mic.onresult = function (intent, entities, response) {
-		var r = kv("intent", intent);
+  var info = function (msg) {
+    document.getElementById("info").innerHTML = msg;
+  };
+  var error = function (msg) {
+    document.getElementById("error").innerHTML = msg;
+  };
+  mic.onready = function () {
+    info("Microphone is ready to record");
+  };
+  mic.onaudiostart = function () {
+    info("Recording started");
+    error("");
+  };
+  mic.onaudioend = function () {
+    info("Recording stopped, processing started");
+  };
+  mic.onresult = function (intent, entities) {
+    var r = kv("intent", intent);
 
-		for (var k in entities) {
-			var e = entities[k];
+    for (var k in entities) {
+      var e = entities[k];
 
-			if (!(e instanceof Array)) {
-				r += kv(k, e.value);
-			} else {
-				for (var i = 0; i < e.length; i++) {
-					r += kv(k, e[i].value);
-				}
-			}
-		}
-    $scope.search = response.msg_body;
-    $scope.query();
+      if (!(e instanceof Array)) {
+        r += kv(k, e.value);
+      } else {
+        for (var i = 0; i < e.length; i++) {
+          r += kv(k, e[i].value);
+        }
+      }
+    }
+
+    document.getElementById("result").innerHTML = r;
+  };
+  mic.onerror = function (err) {
+    error("Error: " + err);
+  };
+	mic.onconnecting = function () {
+		info("Microphone is connecting");
 	};
 	mic.ondisconnected = function () {
+    info("Microphone is not connected");
     mic.connect("VF37BMDRZO74V4XNSGLDRCCR6LZS2MQD");
 	};
 
 	mic.connect("VF37BMDRZO74V4XNSGLDRCCR6LZS2MQD");
 
-	function kv (k, v) {
-		if (toString.call(v) !== "[object String]") {
-			v = JSON.stringify(v);
-		}
-		return k + "=" + v + "\n";
-	}
-
+  function kv (k, v) {
+    if (toString.call(v) !== "[object String]") {
+      v = JSON.stringify(v);
+    }
+    return k + "=" + v + "\n";
+  }
 }]);
