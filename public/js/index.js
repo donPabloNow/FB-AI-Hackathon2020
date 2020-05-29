d5fb663a5798e08cb4966510487c6768176af534
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-05-29 10:39:50 -0400

    more separation of concerns w/ mic

--- public/js/index.js
+++ public/js/index.js
@@ -165,52 +165,6 @@ app.controller("mainController", ['$scope','$http','$sce', function($scope, $htt
       }
     })
   }
-
-  $scope.initMic = function() {
-    var mic = new Wit.Microphone(document.getElementById("microphone"));
-    var info = function (msg) {
-      if(document.getElementById("info")){
-        document.getElementById("info").innerHTML = msg;
-      }
-    };
-    var error = function (msg) {
-      if(document.getElementById("error")){
-        document.getElementById("error").innerHTML = msg;
-      }
-    };
-    mic.onready = function () {
-      info("Click the microphone to record");
-    };
-    mic.onaudiostart = function () {
-      info("Recording started");
-      error("");
-    };
-    mic.onaudioend = function () {
-      info("Recording stopped, processing started");
-    };
-    mic.onresult = function (intent, entities, res) {
-      console.log(res.msg_body);
-      if(res.msg_body) {
-        $scope.search = res.msg_body;
-        $scope.query()
-      }
-      document.getElementById("result").innerHTML = res.msg_body;
-    };
-    mic.onerror = function (err) {
-      error("Error: " + err);
-    };
-    mic.onconnecting = function () {
-      info("Microphone is connecting");
-    };
-    mic.ondisconnected = function () {
-      info("Microphone is not connected");
-      mic.connect("VF37BMDRZO74V4XNSGLDRCCR6LZS2MQD");
-    };
-
-    mic.connect("VF37BMDRZO74V4XNSGLDRCCR6LZS2MQD");
-  }
-
-
 }]);
 
 
